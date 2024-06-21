963d5c7329bef2df2aef9573682901f63ac9ff8e
der-matt02 <dermatt02@gmail.com>
2024-06-20 20:13:40 -0500

    Agregar archivos de proyecto.

---
+++ Services/PokemonServices.cs
@@ -0,0 +1,24 @@
+﻿using Pokemones.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Pokemones.Services
+{
+    internal class PokemonServices
+    {
+        public async Task<List<PokemonItem>> DevuelveListadoPokemones()
+        {
+            string url = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20";
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync(url);
+            ListPokemons list_pokemons = JsonConvert.DeserializeObject<ListPokemons>(json);
+            return list_pokemons.result;
+        }
+    }
+}
